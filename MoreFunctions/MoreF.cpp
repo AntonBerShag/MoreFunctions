01f095e943b0d9eae7e81e2ecb1dc8e681087265
Berd_ur18@itstepru.onmicrosoft.com <Berd_ur18@itstepru.onmicrosoft.com>
2022-02-15 21:14:35 +0500

    Решение Задачи1

--- MoreFunctions/MoreF.cpp
+++ MoreFunctions/MoreF.cpp
@@ -4,18 +4,27 @@ using namespace std;
 //inline int func(int num1, int num2);
 //int func(int num1, int num2);
 //double func(double num1, double num2);
-template <typename T1, typename T2> T1 maxFunc(T1 num1, T2 num2) {
-	return num1 > num2 ? num1 : num2;
+template <typename T> T mean(T array[], int length) {
+	T sum = 0;
+	for (int i = 0; i < length; i++)
+		sum += array[i];
+	return sum / length;
 }
 
 int main() {
 	setlocale(LC_ALL, "Russian");
 	float n, m;
-	cin >> n >> m;
+	//cin >> n >> m;
 	/*cout << func(n, m) << endl;
 	cout << func(5.5, 9.5) << endl;*/
-	cout << maxFunc(n, m) << endl;
 	cout << "Топ 1!\n\n";
+
+	//Задача 1
+	cout << "Задача 1\nИзначальный массив:\n";
+	float z1[5] = { 4.1, 4.9, 2.02, 0.101, 1.2 };
+	for (int i = 0; i < 5; i++)
+		cout << z1[i] << " ";
+	cout << "\nСреднее арифметическое элементов массива = " << mean(z1, 5) << endl;
 
 	return 0;
 }
