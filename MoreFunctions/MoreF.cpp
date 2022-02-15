3dda465d6590527b5eac315b2ddd0dbe9d80cec7
admin <admin@DESKTOP-IB66DRU>
2022-02-15 20:33:15 +0500

    Add project files.

---
+++ MoreFunctions/MoreF.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+//inline int func(int num1, int num2);
+//int func(int num1, int num2);
+//double func(double num1, double num2);
+template <typename T1, typename T2> T1 maxFunc(T1 num1, T2 num2) {
+	return num1 > num2 ? num1 : num2;
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	float n, m;
+	cin >> n >> m;
+	/*cout << func(n, m) << endl;
+	cout << func(5.5, 9.5) << endl;*/
+	cout << maxFunc(n, m) << endl;
+
+	return 0;
+}
+
+/*double func(double num1, double num2) {
+	cout << "Дробные числа!\n";
+	return num1 > num2 ? num1 : num2;
+}
+int func(int num1, int num2) {
+	cout << "Целые числа!\n";
+	return num1 > num2 ? num1 : num2;
+}*/
+/*inline int func(int num1, int num2) {
+	return (num1 > num2 ? num1 : num2);
+}*/
